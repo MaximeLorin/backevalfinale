e564782e29a8e1aa694a4677b1e36d4898b31720
mcmicmax <maxime.lorin56@gmail.com>
2022-05-02 09:33:36 +0200

    Initial commit

---
+++ src/main/java/com/zenika/academy/barbajavas/backFinalProject/BackFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zenika.academy.barbajavas.backFinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackFinalProjectApplication.class, args);
+	}
+
+}
